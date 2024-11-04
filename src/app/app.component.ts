c904310e8fa095aacf7ae9cb139cf9e2100c27fb
Epsilon-7 <ramsey11bryan@gmail.com>
2024-11-03 20:34:01 -0600

    Add upload functionality
    Implements select & upload buttons and sends file data to pdf_processing_api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  //imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'pdf-upload';
+
+  constructor(
+    private http:HttpClient
+  ){
+
+  }
+  name:string = ''
+  file:any; 
+  getName(name:string) {
+    this.name = name;
+  }
+
+  getFile(event:any) {
+    this.file = event.target.files[0];
+    console.log('file',this.file);
+  }
+
+  submitData(){
+    //formData object
+    let formData = new FormData();
+    formData.set("name",this.name)
+    formData.set("file",this.file)
+
+    //Submit to API
+    this.http.post('http://127.0.0.1:8000/convert', formData).subscribe(
+      (response)=>{}
+    )
+  }
 }
